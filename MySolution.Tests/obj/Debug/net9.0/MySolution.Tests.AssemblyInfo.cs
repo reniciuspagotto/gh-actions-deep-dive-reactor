a10d5c7e07b632293e11d62fa9f3f09f6b336941
Renicius Pagotto <renicius.pagotto@outlook.com>
2025-05-27 18:18:56 -0300

    new

--- MySolution.Tests/obj/Debug/net9.0/MySolution.Tests.AssemblyInfo.cs
+++ MySolution.Tests/obj/Debug/net9.0/MySolution.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySolution.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d983d0e93a05ff471d04ba529d5fe7496049f122")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySolution.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySolution.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
